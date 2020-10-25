5b5e0c137e7a9c4d864d045947cc2291238c87a1
shivam.gupta <shivam.gupta@intimetec.com>
2020-10-25 12:34:19 +0530

    Intial commit for server setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = 5000;
+
+app.use("/api", routes);
+app.get("/", (req, res) => {
+    res.send("I am ready to server requests");
+})
+
+app.listen(port, () => {
+    console.log("Listening on port", port);
+})
